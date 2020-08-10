e3b07f1d9aaf7c40dc3f61e2d76b291cf8568ae3
Sibbngheid <sibbngheid@gmail.com>
2020-08-10 14:14:07 +0300

    fix: type

--- index.ts
+++ index.ts
@@ -1,69 +1,69 @@
-import type { App, ObjectDirective } from 'vue'
+import type { App, ObjectDirective } from "vue";
 
-type Truthy<T> = T extends false | '' | 0 | null | undefined ? never : T
+type Truthy<T> = T extends false | "" | 0 | null | undefined ? never : T;
 
 function isTruthy<T>(value: T): value is Truthy<T> {
-  return !!value
+  return !!value;
 }
 
 export function variantwind(className: string) {
-  let plainClasses = className
+  let plainClasses = className;
 
   // Array of blocks, e.g. ["lg:{bg-red-500 hover:bg-red-900}"]
-  const blocks = className.match(/\w*:\{(.*?)\}/g)
+  const blocks = className.match(/\w*:\{(.*?)\}/g);
 
   if (!blocks) {
-    return plainClasses
+    return plainClasses;
   }
 
   const processedClasses = blocks
     .map((block) => {
-      plainClasses = plainClasses.replace(block, '').trim()
-      const [variant, classes] = block.split(/:(.+)/)
+      plainClasses = plainClasses.replace(block, "").trim();
+      const [variant, classes] = block.split(/:(.+)/);
 
       const withVariants = classes
-        .replace(/\{|\}/g, '')
-        .replace(' ', ' ' + variant + ':')
+        .replace(/\{|\}/g, "")
+        .replace(" ", " " + variant + ":");
 
       return withVariants.startsWith(variant)
         ? withVariants
-        : variant + ':' + withVariants
+        : variant + ":" + withVariants;
     })
-    .join(' ')
+    .join(" ");
 
-  return plainClasses + ' ' + processedClasses
+  return plainClasses + " " + processedClasses;
 }
 
 export const directive: ObjectDirective = {
   beforeMount(el) {
-    el.className = variantwind(el.className)
+    el.className = variantwind(el.className);
   },
   updated(el) {
-    el.className = variantwind(el.className)
+    el.className = variantwind(el.className);
   },
-}
+};
 
 export const extractor = (content: string) => {
-  let extract = [];
-  const match = content.match(/[^<]*[^>]/g)
-  
+  let extract: string[] = [];
+  const match = content.match(/[^<]*[^>]/g);
+
   if (match) {
     extract = match
       .map((item) => item.match(/\w*:\{(.*?)\}/g))
       .filter(isTruthy)
-      .flatMap((classes) => variantwind(classes.join(' ')).trim().split(' '))
+      .flatMap((classes) => variantwind(classes.join(" ")).trim().split(" "));
   }
 
   // Capture as liberally as possible, including things like `h-(screen-1.5)`
-  const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+  const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
 
   // Capture classes within other delimiters like .block(class="w-1/2") in Pug
   const innerMatches =
-    content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || []
+    content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || [];
 
-  return broadMatches.concat(innerMatches, extract)
-}
+  return broadMatches.concat(innerMatches, extract);
+};
 
-export default (app: App, directiveName = 'variantwind') => {
-  app.directive(directiveName, directive)
-}
+export default (app: App, directiveName = "variantwind") => {
+  app.directive(directiveName, directive);
+};
